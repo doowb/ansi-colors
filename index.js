925f1bbd5a53ffe533e11f24fa40c30a2861e083
Brian Woodward <brian.woodward@gmail.com>
2015-11-30 12:00:30 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * ansi-colors <https://github.com/doowb/ansi-colors>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
